25a238e67ac8b47a5444a0069cc3ea8d4af3ba15
Yulia Shcherban <caribywest@gmail.com>
2021-05-21 12:57:03 +0300

    переключатель цветов (код)

--- js/change-color.js
+++ js/change-color.js
@@ -1,40 +1,35 @@
-const colors = [
-  '#FFFFFF',
-  '#2196F3',
-  '#4CAF50',
-  '#FF9800',
-  '#009688',
-  '#795548',
-];
+import colors from './colors.js'
 
 const btnStart = document.querySelector('[data-action="start"]');
 const btnStop = document.querySelector('[data-action="stop"]');
-
 let timerId = null;
 
 btnStart.addEventListener('click', onBtnStart);
 
 function onBtnStart() {
-  btnStart.removeEventListener('click', onBtnStart);
-  btnStop.addEventListener('click', onBtnStop);
-  btnStart.classList.remove('is-active');
-  btnStop.classList.add('is-active');
+  changeActivBtn(btnStart, btnStop);
   timerId = setInterval(changeColor, 1000);
-
-  console.log(`запустили таймер', ${timerId}`);
+  // console.log(`запустили таймер', ${timerId}`);
 };
-
-
 
 function onBtnStop() {
   clearInterval(timerId);
   document.body.style.backgroundColor = '';
-  btnStart.classList.add('is-active');
-  btnStop.classList.remove('is-active');
-  btnStart.addEventListener('click', onBtnStart);
-  btnStop.removeEventListener('click', onBtnStop);
-  
-  console.log('остановили таймер');
+  changeActivBtn(btnStop, btnStart);
+  // console.log('остановили таймер');
+};
+
+function changeActivBtn(activeBtn, noActiveBtn) {
+  if (activeBtn === btnStart) {
+    activeBtn.removeEventListener('click', onBtnStart);
+    noActiveBtn.addEventListener('click', onBtnStop);
+  } else {
+    activeBtn.removeEventListener('click', onBtnStop);
+    noActiveBtn.addEventListener('click', onBtnStart);
+  };
+
+  activeBtn.classList.remove('is-active');
+  noActiveBtn.classList.add('is-active');
 };
 
 // генерирует случайное число(индекс элемента массива цветов)
@@ -43,8 +38,5 @@ const randomIntegerFromInterval = (min, max) => {
 };
 
 function changeColor() {
-  // const currentColor = randomIntegerFromInterval(0, (colors.length - 1));
   document.body.style.backgroundColor = colors[randomIntegerFromInterval(0, (colors.length - 1))];
 };
-
-// const timerId = setInterval(changeColor, 1000);
